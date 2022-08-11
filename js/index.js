d6635e7dc8cb822dea61da8120abfaa51d80efde
kwakjaehun <jh5414092@icloud.com>
2022-08-11 20:45:12 +0900

    create Draw func

--- js/index.js
+++ js/index.js
@@ -5,62 +5,84 @@ const anchor = document.querySelector('#anchor');
 const btn = document.querySelector('#btn');
 let oldCard = document.querySelector('.card');
 
+if(localStorage.getItem('used') === null){
+  localStorage.setItem('used', '0');
+}
+
+
 // add id prop in card
 for (let i=0; i<cards.length; i++){
   const card = cards[i];
-  card.id = i+1;
+  card.id = i+2;
+}
+console.log(cards);
+
+function getRandomNumber(min, max){
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  let num = Math.floor(Math.random() * (max - min) + min);
+  return num;
+
+}
+
+function selectCard(cards, num){
+  let result;
+
+  for (let i=0; i<cards.length; i++){
+    const card = cards[i];
+    if(card.id === num){
+      result = card;
+      break;
+    }
+  }
+  return result;
+}
+
+function createQuizBox(card){
+  const quiz = document.createElement('h2');
+  quiz.className = 'quiz'
+  const quizText = document.createTextNode(card.body);
+  quiz.appendChild(quizText);
+
+  const minValue = document.createElement('p');
+  minValue.className = 'opt'
+  const minText = document.createTextNode('1. ' + card.min);
+  minValue.appendChild(minText);
+
+  const maxValue = document.createElement('p');
+  maxValue.className = 'opt'
+  const maxText = document.createTextNode('10. ' + card.max);
+  maxValue.appendChild(maxText);
+
+  const valueBox = document.createElement('div');
+  valueBox.className = 'opt-box'
+  valueBox.append(minValue, maxValue);
+
+  const newDiv = document.createElement('div');
+  newDiv.className = 'card'
+  newDiv.append(quiz, valueBox);
+  
+  document.body.appendChild(newDiv);
 }
 
 function DrawCard(){
 
-  function getRandomNumber(min, max){
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min);
-  }
-  
-  function selectCard(cards, num){
-    let result;
-  
-    for (let i=0; i<cards.length; i++){
-      const card = cards[i];
-      if(card.id === num){
-        result = card;
-        break;
-      }
-    }
-    return result;
-  }
-  
-  function createQuizBox(card){
-    const quiz = document.createElement('h2');
-    quiz.className = 'quiz'
-    const quizText = document.createTextNode(card.body);
-    quiz.appendChild(quizText);
-  
-    const minValue = document.createElement('p');
-    minValue.className = 'opt'
-    const minText = document.createTextNode('1. ' + card.min);
-    minValue.appendChild(minText);
-  
-    const maxValue = document.createElement('p');
-    maxValue.className = 'opt'
-    const maxText = document.createTextNode('10. ' + card.max);
-    maxValue.appendChild(maxText);
-  
-    const valueBox = document.createElement('div');
-    valueBox.className = 'opt-box'
-    valueBox.append(minValue, maxValue);
-  
-    const newDiv = document.createElement('div');
-    newDiv.className = 'card'
-    newDiv.append(quiz, valueBox);
-    
-    document.body.appendChild(newDiv);
-  }
-  
   // Generate card id
-  let cardNum = getRandomNumber(1, cards.length+1);
+  let usedNumbers = localStorage.getItem('used');
+  
+  let cardNum = getRandomNumber(2, cards.length+2);
+  if (usedNumbers.split(',').length < cards.length+1){
+    // generate Number
+    while(usedNumbers.includes(cardNum)){
+    cardNum = getRandomNumber(2, cards.length+2);
+    }
+  } else {
+    // clear localStorage
+    alert("You all drawed cards!");
+  }
+  
+  localStorage.setItem('used', `${usedNumbers},${cardNum}`);
+  console.log(localStorage);
   
   const selectedCard = selectCard(cards, cardNum);
   
